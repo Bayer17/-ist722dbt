24996045230ca75c64ebc44ad490d6098c14dcf0
Alex Bayer <ambayer@g.syr.edu>
2024-03-21 20:10:33 +0000

    fghf

--- northwind/models/obt_sales.sql
+++ northwind/models/obt_sales.sql
@@ -1,4 +1,4 @@
-with fact_sales as (
+with f_sales as (
     select * from {{ ref('fact_sales') }}
 ),
 d_customer as (
@@ -14,8 +14,8 @@ select
     d_customer.*, 
     d_employee.*, 
     d_date.*,
-    fact_sales.*
-    from fact_sales as f
+    f_sales.*
+    from f_sales as f
     left join d_customer on f.customerkey = d_customer.customerkey
     left join d_employee on f.employeekey = d_employee.employeekey
     left join d_date on f.saledatekey = d_date.datekey
